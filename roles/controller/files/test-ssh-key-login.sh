93425ca45204f62dc8e3647d2db1820c5b30ceb7
James Corbett <james@jrc313.com>
2019-12-27 12:15:03 +0000

    Add nodes playbook
    - Install Ansible server ssh key on nodes
    - Split common tasks into separate files
    - Disable StrictHostKeyChecking in test-ssh-key-login.sh to prevent the authenticity question from popping up

--- roles/controller/files/test-ssh-key-login.sh
+++ roles/controller/files/test-ssh-key-login.sh
@@ -3,7 +3,7 @@
 host=$1
 keyfile=$2
 
-ssh -i "$keyfile" -o "IdentitiesOnly yes" -o "PreferredAuthentications publickey" -o "ControlMaster no" "$host" exit 2>/dev/null
+ssh -i "$keyfile" -o "IdentitiesOnly yes" -o "PreferredAuthentications publickey" -o "ControlMaster no" -o "StrictHostKeyChecking no" "$host" exit 2>/dev/null
 if [ "$?" = "0" ]; then
     echo "yes"
 else
